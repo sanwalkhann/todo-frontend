ad135f9decb8a0f8f57ae5d9842bb791577aa410
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-12 11:40:44 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import TodoApp from './components/todos'; 
-// import TodoList from './components/todoList'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
